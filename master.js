8af100479a57fcd036665bfbd6bc4955745cdaf4
Pavel <pkrok01@outlook.com>
2020-06-24 17:30:51 -0700

    have jebait elements appear once when released instead of every loop

--- master.js
+++ master.js
@@ -1,7 +1,8 @@
 let proxyUrl = 'https://cors-anywhere0.herokuapp.com/',
     targetUrl = 'https://hub.spigotmc.org/jenkins/view/RSS/job/Bukkit-RSS/api/json',
     nextBuild = 723
-    $jebait = $('#jebait');
+    $jebait = $('#jebait'),
+    bool = true;
 
 checkNextBuild(nextBuild);
 setTime();
@@ -27,11 +28,15 @@ function checkNextBuild(buildNum) {
         console.log('BROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO IT\'S OUTsdfffffffffffffbdxkjhbfgsdkjbjkfshdbkjfbhdksjfbsdkjbsdkjhbsdkjfbsdkhbsdkbfsdkjkksfdkhbjdsf');
         
         $('#audio-jebait')[0].play();
-        $jebait.append($('<button>').text('Pause Audio').click(()=>$('#audio-jebait')[0].pause()));
-        $jebait.append('<br>')
-        for (let i = 0; i < 45; i++) {
-          $jebait.append($('<img>').attr('src','https://cdn.betterttv.net/emote/5b1740221c5a6065a7bad4b5/3x'));
+        if (bool) {
+            $jebait.append($('<button>').text('Pause Audio').click(()=>$('#audio-jebait')[0].pause()));
+            $jebait.append('<br>')
+            for (let i = 0; i < 45; i++) {
+              $jebait.append($('<img>').attr('src','https://cdn.betterttv.net/emote/5b1740221c5a6065a7bad4b5/3x'));
+            }
+            bool = false;
         }
+
     });
 }
 
